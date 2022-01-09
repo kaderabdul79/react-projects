8a94ddae86fa32a25fd47f3c14032c5f8c500117
kaderabdul <kaderabdul7980@gmail.com>
2022-01-10 00:09:36 +0600

    add boostrap class in student component

--- src/pages/Student/Student.js
+++ src/pages/Student/Student.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Student = (props) => {
     const {id,name,email} = props.student
     return (
-        <div>
-            {id}
-            <h3>{name}</h3>
-            <h3>{email}</h3>
+        <div className="card" style={{w8idth: '1rem',display:'flex',backgroundColor:'tan'}}>
+        <div className="card-body" >
+            <h5 className="card-title">{id}</h5>
+            <p className="card-text">{email}</p>
+            <p className="card-text">{name}</p>
+            <Link to=""><button className="btn btn-primary">View</button></Link>
+            <Link to=""><button className="btn btn-secondary">Edit</button></Link>
+            <Link to=""><button className="btn btn-danger">Delete</button></Link>
         </div>
+    </div>
     );
 };
 
