2a6b2cde92832b5473ec055b9720017f44c0f76c
kaderabdul <kaderabdul7980@gmail.com>
2022-01-13 23:52:09 +0600

    add student from dashboard

---
+++ src/pages/AddStudent/AddStudent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddStudent = () => {
+    return (
+        <div>
+            <form>
+                <input type="email" name="email" placeholder="name@example.com" /><br />
+            </form>
+        </div>
+    );
+};
+
+export default AddStudent;
